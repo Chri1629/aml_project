5567a95ecc7c0d8ea91d68510e368037bf42bf9b
Chri1629 <christian.uccheddu@gmail.com>
2020-07-24 16:07:48 +0200

    Change histo plot to do comparison and to save the plot

--- preprocessing/explorative_plots.py
+++ preprocessing/explorative_plots.py
@@ -4,19 +4,22 @@ File che definisce i plot esplorativi
 
 import matplotlib.pyplot as plt
 
-def histogram_plot(x):
+def histogram_plot(x,y):
 
     '''
     Istogramma per disegnare le distribuzioni delle variabili
     @params:
         x:   - Required   : Serie di cui disegnare la distribuzione
-    @return:
-    	l: lista di directory
     '''
 
-    plt.figure()
+    fig = plt.figure()
     plt.hist(x, bins = 1000,  color = "skyblue")
+    plt.hist(y, bins = 1000,  color = "red")
     plt.xlim(min(x), max(x))
+    plt.xlabel("Bins di f{}".format(x.name))
+    plt.ylabel("Count")
     plt.grid()
-    plt.title('Distribuzione di f{} '.format(x.name))
+    plt.title('Distribuzione di f{}'.format(x.name))
+    plt.legend()
     plt.show()
+    fig.savefig("preprocessing/pics/Histogram_f{}.png".format(x.name), format = "png", dpi = 200)
--- preprocessing/open.py
+++ preprocessing/open.py
@@ -1,13 +1,20 @@
+'''
+    Programma che apre tutti i file e ne fa un primo preprocessing
+'''
+
 import pandas as pd
 from .explorative_plots import histogram_plot
 
 def open_data():
+    '''
+    Funzione che carica preliminarmente i dataset e li esplora
+    '''
 
     # Change the path to the dataset
     path = "/home/christian/Scrivania/progetti/aml_project/data/"
 
     #sample = pd.read_csv("data/sample_submission_V2.csv", sep = ",")
     train = pd.read_csv(path + "train_V2.csv", sep = ",")
-    #test = pd.read_csv("data/test_V2.csv", sep = ",")
+    test = pd.read_csv("data/test_V2.csv", sep = ",")
 
-    histogram_plot(train['winPlacePerc'])
+    histogram_plot(train['matchDuration'], test['matchDuration'])
